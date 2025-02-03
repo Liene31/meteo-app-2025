3555ee62d19c7ccba17117b7a3b04113f9c60a95
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-02-03 21:22:38 +0100

    Moved API call into SearchCity function

--- src/index.js
+++ src/index.js
@@ -47,8 +47,12 @@ currentDateElement.innerHTML = showCurrentDate(currentDate);
 function searchCity(event) {
   event.preventDefault();
   let searchValue = document.querySelector("#search-value");
-  let cityElement = document.querySelector("h1");
-  cityElement.innerHTML = searchValue.value;
+  city = searchValue.value;
+
+  let apiKey = `9a5034o3fd0ad6bc1bata98ee2fcd7b0`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showCurrentMeteo);
 }
 
 let searchButton = document.querySelector("#search-btn");
@@ -57,14 +61,13 @@ searchButton.addEventListener("click", searchCity);
 // Updates current weather
 
 function showCurrentMeteo(response) {
-  console.log(response.data);
-
   let currentTemp = Math.round(response.data.temperature.current);
   let feelsLikeTemp = Math.round(response.data.temperature.feels_like);
   let description = response.data.condition.description;
   let windSpeed = response.data.wind.speed;
   let pressure = response.data.temperature.pressure;
   let humidity = response.data.temperature.humidity;
+  let city = response.data.city;
 
   let currentTempElement = document.querySelector(".current-temp-value");
   currentTempElement.innerHTML = currentTemp;
@@ -83,10 +86,7 @@ function showCurrentMeteo(response) {
 
   let humidityElement = document.querySelector(".humidity");
   humidityElement.innerHTML = humidity;
+
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = city;
 }
-
-let city = "Oslo";
-let apiKey = `9a5034o3fd0ad6bc1bata98ee2fcd7b0`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(showCurrentMeteo);
